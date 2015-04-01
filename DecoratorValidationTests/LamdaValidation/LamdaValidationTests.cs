388f6beba2fc358fe0bf7c19e2437233b5a38ca9
Killfile <thegreatkillfile@gmail.com>
2015-04-01 00:11:08 -0400

    Added Lamba Validator with Unit Tests

---
+++ DecoratorValidationTests/LamdaValidation/LamdaValidationTests.cs
@@ -0,0 +1,53 @@
+﻿using DecoratorValidation.Core;
+using DecoratorValidation.LamdaValidation.Validators;
+using NUnit.Framework;
+using System;
+using System.Text;
+
+namespace DecoratorValidationTests.LamdaValidation
+{
+    [TestFixture]
+    public class LamdaValidationTests
+    {
+        private StringBuilder errorAccumulator;
+
+        [SetUp]
+        public void Setup()
+        {
+            errorAccumulator = new StringBuilder();
+        }
+
+        [Test]
+        public void LamdaValidator_Validates_Fail()
+        {
+            string errorMessage = "Lamda not met";
+            Validator<Guid> validator = new ValidatorBaseCase<Guid>();
+            Guid expectedValue = Guid.NewGuid();
+            validator = new LamdaValidator<Guid>(validator, g => g == expectedValue, errorMessage);
+            bool actual = validator.Validate(Guid.NewGuid(), errorAccumulator);
+            Assert.That(actual, Is.False);
+        }
+
+        [Test]
+        public void LamdaValidator_Validates_Pass()
+        {
+            string errorMessage = "Lamda not met";
+            Validator<Guid> validator = new ValidatorBaseCase<Guid>();
+            Guid expectedValue = Guid.NewGuid();
+            validator = new LamdaValidator<Guid>(validator, g => g == expectedValue, errorMessage);
+            bool actual = validator.Validate(expectedValue, errorAccumulator);
+            Assert.That(actual, Is.True);
+        }
+
+        [Test]
+        public void LamdaValidator_Validates_FailProducesError()
+        {
+            string errorMessage = "Lamda not met";
+            Validator<Guid> validator = new ValidatorBaseCase<Guid>();
+            Guid expectedValue = Guid.NewGuid();
+            validator = new LamdaValidator<Guid>(validator, g => g == expectedValue, errorMessage);
+            bool actual = validator.Validate(Guid.NewGuid(), errorAccumulator);
+            Assert.That(errorAccumulator.ToString(), Is.EqualTo(errorMessage));
+        }
+    }
+}
