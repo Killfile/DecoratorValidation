5a2f0eb625156215171949ffb16c0c1d3377b890
Killfile <thegreatkillfile@gmail.com>
2015-03-31 23:40:31 -0400

    Added tests for int and bool validation, which are silly

---
+++ DecoratorValidationTests/BoolValidation/BoolValidator_ExpectedValueTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DecoratorValidation.BoolValidation.Validators;
+using NUnit.Framework;
+using DecoratorValidation.Core;
+namespace DecoratorValidation.BoolValidation.Validators.Tests
+{
+    [TestFixture()]
+    public class BoolValidator_ExpectedValueTests
+    {
+        private StringBuilder errorAccumulator;
+
+        [SetUp]
+        public void Setup()
+        {
+            errorAccumulator = new StringBuilder();
+        }
+
+        [Test()]
+        [TestCase(true, true, true)]
+        [TestCase(true, false, false)]
+        [TestCase(false, true, false)]
+        [TestCase(false, false, true)]
+        public void WhenBoolValidator_ExpectedValueValidates(bool toValidate, bool expectedValue, bool expectedResult)
+        {
+            string errorMessage = "BoolValidator_ExpectedValue Failed";
+            Validator<bool> validator = new ValidatorBaseCase<bool>();
+            validator = new BoolValidator_ExpectedValue(validator, expectedValue, errorMessage);
+            bool actual = validator.Validate(toValidate, errorAccumulator);
+            Assert.That(actual, Is.EqualTo(expectedResult));
+        }
+
+        [Test()]
+        [TestCase(true, false)]
+        public void WhenBoolValidator_ExpectedValueFails_ErrorMessageIsGenerated(bool toValidate, bool expectedValue)
+        {
+            string errorMessage = "BoolValidator_ExpectedValue Failed";
+            Validator<bool> validator = new ValidatorBaseCase<bool>();
+            validator = new BoolValidator_ExpectedValue(validator, expectedValue, errorMessage);
+            bool actual = validator.Validate(toValidate, errorAccumulator);
+            Assert.That(errorAccumulator.ToString(), Is.EqualTo(errorMessage));
+        }
+    }
+}
