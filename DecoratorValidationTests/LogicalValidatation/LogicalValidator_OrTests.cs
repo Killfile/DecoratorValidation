d9ce04f9ab383fa254f52c7bf835d7c32680af5e
Killfile <thegreatkillfile@gmail.com>
2015-04-06 22:41:46 -0400

    Added a branching logical validator (or)

---
+++ DecoratorValidationTests/LogicalValidatation/LogicalValidator_OrTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DecoratorValidation.Core;
+using DecoratorValidation.BoolValidation.Validators;
+using DecoratorValidation.LogicalValidation;
+using Moq;
+
+namespace DecoratorValidationTests.LogicalValidatation
+{
+    [TestClass]
+    public class LogicalValidator_OrTests
+    {
+        [TestMethod]
+        public void CanConstructOrValidator()
+        {
+            Validator<bool> left = new ValidatorBaseCase<bool>();
+            left = new BoolValidator_ExpectedValue(left, true, "Not true");
+
+            Validator<bool> right = new ValidatorBaseCase<bool>();
+            right = new BoolValidator_ExpectedValue(right, true, "Not true");
+
+            Validator<bool> logicalOr = new ValidatorBaseCase<bool>();
+            logicalOr = new LogicalValidator_Or<bool>(logicalOr, left, right, "Neither passed");
+        }
+
+        [TestMethod]
+        public void MockedValidatorCanBeMadeToAlwaysFail()
+        {
+            Validator<bool> validator = GenerateFailingValidator();
+            bool isValid = validator.Validate(true);
+            Assert.IsFalse(isValid);
+        }
+
+        private static Validator<bool> GenerateFailingValidator()
+        {
+            var validatorMock = new Mock<Validator<bool>>();
+            validatorMock.Setup(m => m.Validate(It.IsAny<bool>())).Returns(false);
+            Validator<bool> validator = validatorMock.Object;
+            return validator;
+        }
+
+        private static Validator<bool> GeneratePassingValidator()
+        {
+            var validatorMock = new Mock<Validator<bool>>();
+            validatorMock.Setup(m => m.Validate(It.IsAny<bool>())).Returns(true);
+            Validator<bool> validator = validatorMock.Object;
+            return validator;
+        }
+
+        [TestMethod]
+        public void OrValidatorReturnsTrueIfOnlyLeftValidates()
+        {
+            Validator<bool> left = GeneratePassingValidator();
+
+            Validator<bool> right = GenerateFailingValidator();
+
+            Validator<bool> logicalOr = new ValidatorBaseCase<bool>();
+            logicalOr = new LogicalValidator_Or<bool>(logicalOr, left, right, "Neither passed");
+            Assert.IsTrue(logicalOr.Validate(true));
+        }
+
+        [TestMethod]
+        public void OrValidatorReturnsTrueIfOnlyRightValidates()
+        {
+            Validator<bool> right = GeneratePassingValidator();
+
+            Validator<bool> left = GenerateFailingValidator();
+
+            Validator<bool> logicalOr = new ValidatorBaseCase<bool>();
+            logicalOr = new LogicalValidator_Or<bool>(logicalOr, left, right, "Neither passed");
+            Assert.IsTrue(logicalOr.Validate(true));
+        }
+
+        [TestMethod]
+        public void OrValidatorReturnsTrueIfBothValidate()
+        {
+            Validator<bool> left = GeneratePassingValidator();
+
+            Validator<bool> right = GeneratePassingValidator();
+
+            Validator<bool> logicalOr = new ValidatorBaseCase<bool>();
+            logicalOr = new LogicalValidator_Or<bool>(logicalOr, left, right, "Neither passed");
+            Assert.IsTrue(logicalOr.Validate(true));
+        }
+
+        [TestMethod]
+        public void OrValidatorReturnsFalseIfNeitherValidates()
+        {
+            Validator<bool> left = GenerateFailingValidator();
+
+            Validator<bool> right = GenerateFailingValidator();
+
+            Validator<bool> logicalOr = new ValidatorBaseCase<bool>();
+            logicalOr = new LogicalValidator_Or<bool>(logicalOr, left, right, "Neither passed");
+            Assert.IsFalse(logicalOr.Validate(true));
+        }
+    }
+}
---
+++ LogicalValidation/LogicalValidator_Or.cs
@@ -0,0 +1,30 @@
+﻿using DecoratorValidation.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorValidation.LogicalValidation
+{
+    public class LogicalValidator_Or<T> : ValidatorDecorator<T> 
+    {
+        private string _errorMessage;
+        private Validator<T> _left;
+        private Validator<T> _right;
+
+        public LogicalValidator_Or(Validator<T> a, Validator<T> left, Validator<T> right, String errorMessage) : base(a)
+        {
+            _errorMessage = ErrorMessage;
+            _left = left;
+            _right = right;
+        }
+
+        public override bool Validate(T toValidate)
+        {
+            isValid = _left.Validate(toValidate) || _right.Validate(toValidate);
+            AppendErrorMessage(_errorMessage);
+            return isValid && base.Validate(toValidate);
+        }
+    }
+}
