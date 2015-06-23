2502424a2a6307253d4c9293fce2f2796d1f3974
Killfile <thegreatkillfile@gmail.com>
2015-06-22 21:09:00 -0400

    Add EndsWith string validation

---
+++ DecoratorValidationTests/StringValidation/StringValidator_EndsWithTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DecoratorValidation.StringValidation.Validators;
+using NUnit.Framework;
+using DecoratorValidation.Core;
+namespace DecoratorValidation.StringValidation.Validators.Tests
+{
+    [TestFixture()]
+    public class StringValidator_EndsWithTests
+    {
+        private StringBuilder errorAccumulator;
+
+        [SetUp]
+        public void Setup()
+        {
+            errorAccumulator = new StringBuilder();
+        }
+
+        [Test()]
+        [TestCase("ABC", "BC", true, Result = true)]
+        [TestCase("ABC", "bc", true, Result = false)]
+        [TestCase("ABC", "AB", true, Result = false)]
+        [TestCase("ABC", "ABCD", true, Result = false)]
+        [TestCase("ABCABC", "ABCABC", true, Result = true)]
+        [TestCase("ABC", "bc", true, Result = false)]
+        [TestCase("ABCABC", "abcabc", true, Result = false)]
+        [TestCase("ABC", "bc", false, Result = true)]
+        [TestCase("ABCABC", "abcabc", false, Result = true)]
+        public bool WhenEndsWithValidates(string candidate, string EndsWith, bool caseSensitive)
+        {
+            string error = "EndsWith not met";
+            Validator<String> validator = new ValidatorBaseCase<String>();
+            validator = new StringValidator_EndsWith(validator, EndsWith, error, caseSensitive);
+            return validator.Validate(candidate);
+        }
+
+        [Test()]
+        public void WhenEndsWithDoesNotValidate_GenerateError()
+        {
+            string errorMessage = "EndsWith not found";
+            string candidate = "abcabc";
+            string EndsWith = "D";
+            Validator<String> validator = new ValidatorBaseCase<String>();
+            validator = new StringValidator_EndsWith(validator, EndsWith, errorMessage);
+            bool result = validator.Validate(candidate);
+            Assert.That(validator.ErrorMessage, Is.EqualTo(errorMessage));
+        }
+    }
+}
---
+++ StringValidation/StringValidator_EndsWith.cs
@@ -0,0 +1,29 @@
+﻿using DecoratorValidation.Core;
+using System;
+using System.Text;
+
+namespace DecoratorValidation.StringValidation.Validators
+{
+    public class StringValidator_EndsWith : ValidatorDecorator<String>
+    {
+        private readonly String _expectedString;
+        private readonly String _errorMessage;
+        private readonly bool _isCaseSensitive;
+
+        public StringValidator_EndsWith(Validator<String> a, String expectedString, String errorMessage, bool isCaseSensitive = true)
+            : base(a)
+        {
+            _expectedString = expectedString;
+            _errorMessage = errorMessage;
+            _isCaseSensitive = isCaseSensitive;
+        }
+
+        public override bool Validate(String toValidate)
+        {
+            isValid =  _isCaseSensitive ? toValidate.EndsWith(_expectedString) : toValidate.ToLower().EndsWith(_expectedString.ToLower());
+            AppendErrorMessage(_errorMessage);
+
+            return isValid && base.Validate(toValidate);
+        }
+    }
+}
